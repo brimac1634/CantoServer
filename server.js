8a479589e563d1f674f6bba5616f68982627d668
Brian MacPherson <brimac1634@gmail.com>
2019-02-01 17:02:43 +0800

    added signin endpoint

--- server.js
+++ server.js
@@ -32,7 +32,23 @@ app.post('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-
+	const { email, password } = req.body;
+	db.select('email', 'hash').from('login')
+		.where('email', '=', email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(password, data[0].hash)
+			if (isValid) {
+				return db.select('*').from('users')
+					.where('email', '=', email)
+					.then(user => {
+						res.json(user[0])
+					})
+					.catch(err => res.status(400).json('unable to retrieve user'))
+			} else {
+				res.status(400).json('wrong credentials')
+			}
+		})
+		.catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
