e9f04392011b91f2b1e2f3a975293bb321f777e5
Brian MacPherson <brimac1634@gmail.com>
2019-06-21 00:35:22 +0800

    added more to stream

--- controllers/stream.js
+++ controllers/stream.js
@@ -1,7 +1,33 @@
 const { validateEmail, sendMail } = require('../helpers/utils');
+const s3 = require('s3');
+
+
+const client = s3.createClient({  
+  s3Options: {
+    accessKeyId: 'AKIAJ2BF62TKDGACU4RA',
+    secretAccessKey: <YOUR SECRET KEY>
+  }
+});
 
 const handleStream = (req, res) => {
-	
+	const params = {
+	    Bucket: 'New-Bucket-1020',
+	    Key: 'test.mp3'
+	}
+	const downloadStream = client.downloadStream(params)
+
+	downloadStream.on('error', function() {
+	  res.status(404).send('Not Found');
+	});
+
+	downloadStream.on('httpHeaders', function(statusCode, headers, resp) {
+	  res.set({
+	    'Content-Type': headers['content-type']
+	  });
+	})
+
+	// Pipe download stream to response
+	downloadStream.pipe(res);
 }
 
 
