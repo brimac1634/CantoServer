ed3b5d3a662fa5177ee0faeeb3b19cb7675c3972
Brian MacPherson <brimac1634@gmail.com>
2019-07-10 14:23:51 +0800

    added back middleware

--- server.js
+++ server.js
@@ -81,10 +81,9 @@ app.post('/get-decks-id', middleware.checkToken, (req, res) => { learn.getDecks(
 
 app.post('/search-decks-id', middleware.checkToken, (req, res) => { learn.searchDecks(req, res, db)})
 
-// , middleware.checkToken
-app.post('/new-deck', (req, res) => { learn.newDeck(req, res, db)})
+app.post('/new-deck', middleware.checkToken, (req, res) => { learn.newDeck(req, res, db)})
 
-app.post('/add-to-deck', (req, res) => { learn.addToDeck(req, res, db)})
+app.post('/add-to-deck', middleware.checkToken, (req, res) => { learn.addToDeck(req, res, db)})
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`app is running on port ${process.env.PORT}`);
